0e5a413b1ffa9aca8edfba4121c7b5c5b9af472f
asrulaiqal <45942446+asrulaiqal@users.noreply.github.com>
2018-12-18 11:43:30 +0800

    Add files via upload

--- Webtech Project/myScript.js
+++ Webtech Project/myScript.js
@@ -11,3 +11,11 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(3.2505,101.7347),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
